339262c4270fa0eb1a6427de00e2c2d30434babb
hiep294 <quanghiep294@gmail.com>
2020-05-28 17:10:23 +0700

    maintain

--- src/components/CustomForm/CustomInput.tsx
+++ src/components/CustomForm/CustomInput.tsx
@@ -1,24 +1,20 @@
 import React from "react";
 import { Input } from "antd";
+import { InputProps } from "antd/lib/input";
 
-type CustomInputType = typeof Input & {
+interface CustomInputInterface extends InputProps {
   label?: string;
-};
+}
 
 const CustomInput = ({
-  label = "Label",
   placeholder = "Design: a tag div of label above this Input tag, does not need to pass any ID",
-  id = "basic",
-  // does not need to pass a value if using Form
-  value = "",
-  // does not need to pass a onChange if using Form
-  onChange = () => {},
-  ...rest
-}: CustomInputType) => {
+  label = "label",
+  ...otherProps
+}: CustomInputInterface) => {
   return (
     <div style={{ position: "relative" }}>
       <label
-        htmlFor={id}
+        htmlFor={otherProps.id}
         style={{ position: "absolute", zIndex: 2, paddingLeft: "11px" }}
       >
         {label}
@@ -26,10 +22,7 @@ const CustomInput = ({
       <Input
         placeholder={placeholder}
         style={{ paddingTop: "20px" }}
-        id={id}
-        value={value}
-        onChange={onChange}
-        {...rest}
+        {...otherProps}
       />
     </div>
   );
